4179fa2fd3893e399c5c29223a2764524cb42137
priti0011 <84434081+priti0011@users.noreply.github.com>
2022-09-19 16:41:42 +0530

    Fetched Attendence, Present,Absent APIs

--- src/Component/Admin/Attendance.jsx
+++ src/Component/Admin/Attendance.jsx
@@ -1,8 +1,62 @@
 import React from "react";
 // import "./../../css/attendence.css";
+import { useState, useEffect } from "react";
 import "./../../css/attendence.scss";
-
+import axios from "axios";
 const Attendance = () => {
+  const [present, setPresent] = useState([]);
+  const [absent, setAbsent] = useState([]);
+  const [allAttendance, setAllAttendance] = useState([]);
+
+  const getAllAttandace = async () => {
+    try {
+      let res = await axios.get("http://localhost:8000/employee/attendance", {
+        params: {
+          eod_date: '2022/09/13',
+        },
+      });
+      setAllAttendance(res.data);
+      console.log("----- All Attandance-Data ------");
+      console.log(allAttendance);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const getPresent = async () => {
+    try {
+      let res = await axios.get(
+        "http://localhost:8000/employee/attendance/present",
+        {
+          params: {
+            eod_date: "2022/09/13",
+          },
+        }
+      );
+      setPresent(res.data);
+      console.log("----- Present-Data  ------");
+      console.log(present);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const getAbsent = async () => {
+    try {
+      let res = await axios.get(
+        "http://localhost:8000/employee/attendance/absent"
+      );
+      setAbsent(res.data);
+      console.log("----- Absent-Data ----");
+      console.log(absent);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPresent();
+    getAbsent();
+    getAllAttandace();
+  }, []);
   return (
     <>
       <div className="row col-12 mx-0 px-0 text-center border-bottom">
