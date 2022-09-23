6c6475163c2e7f286ddfa4c4a89a8f2892fe6f5e
janakOrdex <janak.koradiya@ordextechnology.com>
2022-09-23 17:09:05 +0530

    Implementation of Configuration page at Employee Module

--- src/Component/Admin/History.jsx
+++ src/Component/Admin/History.jsx
@@ -1,98 +1,180 @@
 import React from "react";
 import _image_75 from "./../../Image/75.jpg";
 import "./../../css/admin-history.css";
+import edit_emp from "./../../Image/EditIcon.svg";
 
 const History = () => {
   return (
     <>
-      <div className="row col-12 mx-0 px-0 text-center border-bottom">
-        <h3 className="text-uppercase">end of day history report</h3>
-      </div>
-      <div className="row col-12 mx-0 px-0 justify-content-center border-bottom">
-        <div className="col-12 col-lg-3 bg-img">
-          <img src={_image_75} alt="" />
-          <div className="Empname text-center mt-3">
-            <h5>Employee Name</h5>
-            <p className="mb-0">Employee Profile</p>
+      <div className="container-fluid">
+        <div className="row flex-nowrap">
+          <div className="col py-2 h-100">
+            <div className="row col-12 mx-0 px-0 text-center border-bottom">
+              <h3 className="text-uppercase">end of day report</h3>
+            </div>
           </div>
         </div>
-        <div className="col-12 col-lg-9">
-          <div className="row col-12 mx-0 px-0 d-flex justify-content-center justify-content-md-between">
-            <div className="col-12 col-md-8">
-              <form className="admin-eod">
-                <div className="row col-12 mx-0 px-0 mb-3">
-                  <div className="col-12 col-sm-6">
-                    <p className="mb-1">Attendence</p>
-                    <input
-                      type="text"
-                      name="emdAttendence"
-                      id="empattendence"
-                      className="form-control"
-                    />
-                  </div>
-                </div>
-                <div className="row col-12 mx-0 px-0 mb-3">
-                  <div className="col-12 col-sm-6">
-                    <p className="mb-1">Total Working Time</p>
-                    <div className="cs-form">
-                      <input
-                        type="time"
-                        className="form-control"
-                        value="hours:minutes"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="row col-12 mx-0 px-0 mt-5">
-                  <div
-                    className="col-12 col-sm-6 tel"
-                    style={{ position: "relative" }}
-                  >
-                    <i className="fas fa-phone"></i>
-                    <input
-                      type="tel"
-                      id="phone"
-                      name="phone"
-                      className="form-control"
-                      placeholder="Mo. Num."
-                      pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                    />
-                  </div>
-                  <div
-                    className="col-12 col-sm-6 email"
-                    style={{ position: "relative" }}
-                  >
-                    <i className="fas fa-envelope"></i>
-                    <input
-                      type="email"
-                      id="email"
-                      pattern=".+@globex\.com"
-                      size="30"
-                      className="form-control"
-                      placeholder="Email"
-                    />
-                  </div>
-                </div>
-              </form>
+        <div className="row col-12 mx-0 px-0 justify-content-center border-bottom pb-3">
+          <div className="col-12 col-sm-6 col-md-4 fs-5 d-flex align-items-center justify-content-center">
+            <i className="fas fa-user"></i>
+            <p className="name mb-0 ms-1">Nehil Vijaybhai Bhakta</p>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 fs-5 mt-2 mt-sm-0 d-flex align-items-center justify-content-center">
+            <i className="fas fa-phone"></i>
+            <p className="phone mb-0 ms-1"><span>+91</span> 9987956703</p>
+          </div>
+          <div className="col-12 col-md-4 fs-5 d-flex align-items-center justify-content-center mt-2 mt-md-0">
+            <i className="fas fa-envelope"></i>
+            <p className="email mb-0 ms-1">Nehilv2002@gmail.com</p>
+          </div>
+        </div>
+        <div className="mt-3 d-flex justify-content-center justify-content-sm-end">
+          <nav className="date-btn d-flex justify-content-between" id="btn-top">
+            <div className="nav nav-tabs" id="nav-tab" role="tablist">
+              <button className="nav-link btn-1 active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Date</button>
+              <button className="nav-link btn-1" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Date Range</button>
             </div>
-            <div className="col-12 col-md-4 mx-0 px-0 d-flex flex-column align-items-center justify-content-center text-center bg-date">
-              <div className="col-10 eod-date mb-1">
-                <p className="eod-report mb-0 text-white">
-                  End of Day Report of Date
-                </p>
-                <input
-                  type="date"
-                  id="birthday"
-                  name="birthday"
-                  className="form-control p-2"
-                />
-              </div>
-              <div className="col-2 mt-1">
-                <button type="submit" className="btn-search text-white">
-                  Search
-                </button>
+          </nav>
+        </div>
+        <div className="tab-content" id="nav-tabContent">
+          <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+            <div className="row col-12 mx-0 px-0 my-3 text-center">
+              <div className="col-10 col-sm-8 col-md-5 d-flex align-items-end">
+                <div className="col-10 me-2 date-1">
+                  <p className="date-report mb-0 text-white">End of Day Report of Date</p>
+                  <input type="date" id="birthday" name="birthday" className="form-control p-2" />
+                </div>
+                <div className="col-2">
+                  <button type="submit" className="btn-search text-white">Search</button>
+                </div>
               </div>
             </div>
+          </div>
+          <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+            <div className="row col-12 mx-0 px-0 my-3 text-center align-items-end">
+              <div className="col-12 col-sm-5 col-md-4">
+                <div className="col-12 date-1">
+                  <p className="date-report mb-0 text-white">Starting Date</p>
+                  <input type="date" id="birthday" name="birthday" className="form-control p-2" />
+                </div>
+              </div>
+              <div className="col-12 col-sm-5 col-md-4 mt-3 mt-sm-0">
+                <div className="col-12 date-1">
+                  <p className="date-report mb-0 text-white">End Date</p>
+                  <input type="date" className="form-control p-2" />
+                </div>
+              </div>
+              <div className="col-12 col-sm-2 mt-3 mt-sm-0 d-flex justify-content-center justify-content-sm-start">
+                <button type="submit" className="btn-search text-white">Search</button>
+              </div>
+            </div>
+
+          </div>
+
+
+          <div className="table-responsive" style={{ width: "100%" }}>
+            <table className="table border-end-0">
+              <thead>
+                <tr className="border-start">
+                  <th scope="col" className="border-top">
+                    Sr No.
+                  </th>
+                  <th scope="col" className="border-top">
+                    Date
+                  </th>
+                  <th scope="col" className="border-top">
+                    Name
+                  </th>
+                  <th scope="col" className="border-top">
+                    Project
+                  </th>
+                  <th scope="col" className="border-top">
+                    Task
+                  </th>
+                  <th scope="col" className="border-top">
+                    Description
+                  </th>
+                  <th scope="col" className="border-top">
+                    Status
+                  </th>
+                  <th scope="col" className="border-top" style={{ borderRight: "1px solid #dee2e6" }}>
+                    T.W.T
+                  </th>
+                  {/* <th className="border-0" ></th> */}
+                </tr>
+              </thead>
+              <tbody className="">
+                <tr>
+                  <td style={{ borderLeft: "1px solid #dee2e6" }}>a</td>
+                  <td>b</td>
+                  <td>c</td>
+                  <td>d</td>
+                  <td>e</td>
+                  <td>f</td>
+                  <td>g</td>
+                  <td style={{ borderRight: "1px solid #dee2e6" }}>f</td>
+                  {/* <td className="border-0">
+                    <img
+                      src={edit_emp}
+                      alt=""
+                      width={20}
+                      height={20}
+                    // onClick={() => {
+                    //   editEmployee(data.emp_id);
+                    // }}
+                    />
+                  </td> */}
+                </tr>
+              </tbody>
+              {/* <tbody className="">
+                {empData.map((data) => {
+                  return (
+                    <>
+                      <tr className="border-start">
+                        {console.log(data)}
+                        <th scope="row">{data.emp_id}</th>
+                        <td>{data.emp_code}</td>
+                        <td>{data.emp_fname + " " + data.emp_lname}</td>
+                        <td>{data.email}</td>
+
+                        <td>{data.post}</td>
+
+                        <td
+                          style={{ borderRight: "1px solid #dee2e6" }}
+                          className="text-center"
+                        >
+                          {data.status == "ACTIVE" ? (
+                            <i
+                              className="fa-solid fa-circle"
+                              style={{ color: "green", fontSize: "11px" }}
+                              onClick={handlesubmit}
+                              title="Active"
+                            ></i>
+                          ) : (
+                            <i
+                              className="fa-solid fa-circle"
+                              style={{ color: "#fcba03", fontSize: "11px" }}
+                              title="In Active"
+                            ></i>
+                          )}
+                        </td>
+                        <td className="border-0">
+                          <img
+                            src={edit_emp}
+                            alt=""
+                            width={20}
+                            height={20}
+                            onClick={() => {
+                              editEmployee(data.emp_id);
+                            }}
+                          />
+                        </td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </tbody> */}
+            </table>
           </div>
         </div>
       </div>
--- src/Component/Employee/Configuration.jsx
+++ src/Component/Employee/Configuration.jsx
@@ -1,10 +1,121 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 import "./../../css/configuration.scss";
 
 const Configuration = () => {
+
+  //------------ Loader Code Start------------
+  const [loader, setLoader] = useState(false)
+
+  useEffect(() => {
+    setLoader(true)
+    setTimeout(() => {
+      setLoader(false);
+    }, 1500);
+  }, []);
+
+  //------------ Loader Code End ------------
+
+  const getuserDetails = () => {
+    let userData = JSON.parse(localStorage.getItem("userData"));
+    return userData;
+  };
+
+  const todayDate = () => {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
+  }
+
+  const [mentor, setMentor] = useState({
+    email1: null,
+    email2: null,
+    email3: null,
+  })
+
+  const getMentor = async () => {
+    try {
+      let res = await axios.get("http://localhost:8000/additional/mail", {
+        params: {
+          emp_id: getuserDetails().empId,
+        },
+      });
+      if (res.status == 200) {
+        setMentor({ ...mentor, email1: res.data[0].mentor1_email, email2: res.data[0].mentor2_email, email3: res.data[0].mentor3_email })
+      }
+    } catch (error) {
+      msg("error", "error", "Something went wrong")
+    }
+  }
+
+  useEffect(() => {
+    getMentor();
+  }, [])
+
+  const getInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setMentor({ ...mentor, [name]: value });
+  };
+
+  const msg = (type, icon, title) => {
+    Swal.fire({
+      type: type,
+      icon: icon,
+      title: title,
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#06bdff',
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoader(true)
+    if (mentor.email1 && !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mentor.email1))) {
+      setLoader(false)
+      msg("warning", "warning", "Please enter valid email address for mentor 1")
+    }
+    else if (mentor.email2 && !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mentor.email2))) {
+      setLoader(false)
+      msg("warning", "warning", "Please enter valid email address for mentor 2")
+    }
+    else if (mentor.email3 && !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mentor.email3))) {
+      setLoader(false)
+      msg("warning", "warning", "Please enter valid email address for mentor 3")
+    }
+    else {
+      try {
+        const res = await axios.patch("http://localhost:8000/additional/mail", {
+          emp_id: getuserDetails().empId,
+          email1: mentor.email1,
+          email2: mentor.email2,
+          email3: mentor.email3,
+          updated_at: todayDate()
+        });
+
+        if (res.status == 200) {
+          setLoader(false)
+          msg("success", "success", "Configuration done successfully")
+        } else {
+          setLoader(false)
+          msg("error", "error", "Something went wrong")
+        }
+      } catch (err) {
+        setLoader(false)
+        msg("error", "error", "Something went wrong")
+      }
+    }
+  }
+
+
   return (
     <>
+      {loader ? <div className="loadingPopup"></div> : null}
       <div className="row col-12 mx-0 px-0 text-center border-bottom">
         <h3 className="text-uppercase">configuration</h3>
       </div>
@@ -19,7 +130,9 @@ const Configuration = () => {
               type="email"
               className="form-control"
               id="inputEmail"
-              placeholder="Email"
+              placeholder="Email" name="email1"
+              value={mentor.email1}
+              onChange={getInput}
               required
             />
             <i className="fas fa-pen icon"></i>
@@ -34,8 +147,9 @@ const Configuration = () => {
               type="email"
               className="form-control"
               id="inputEmail"
-              placeholder="Email"
-              required
+              placeholder="Email" name="email2"
+              value={mentor.email2}
+              onChange={getInput}
             />
             <i className="fas fa-pen icon"></i>
           </div>
@@ -49,15 +163,16 @@ const Configuration = () => {
               type="email"
               className="form-control"
               id="inputEmail"
-              placeholder="Email"
-              required
+              placeholder="Email" name="email3"
+              value={mentor.email3}
+              onChange={getInput}
             />
             <i className="fas fa-pen icon"></i>
           </div>
         </div>
         <div className="row col-12 mx-0 px-0 mb-3 align-items-center justify-content-center mentor-id">
           <div className="col-5 px-0 ms-5 d-flex justify-content-end">
-            <button type="submit" className="btn-done text-white">
+            <button type="submit" className="btn-done text-white" onClick={handleSubmit}>
               Done
             </button>
           </div>
